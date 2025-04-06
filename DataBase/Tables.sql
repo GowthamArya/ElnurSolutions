9deacb2351af7fedbdda817d3c471b4b00be4184
gowthamarya <gowtham.arya999@gmail.com>
2025-04-06 22:10:15 +0530

    minor fixes..

--- DataBase/Tables.sql
+++ DataBase/Tables.sql
@@ -25,17 +25,15 @@ CREATE TABLE ProductSubCategories (
         ON UPDATE CASCADE
 );
 
+
 CREATE TABLE Products (
     Id INT IDENTITY(1,1) NOT NULL,
     Name NVARCHAR(25) NOT NULL,
     Description NVARCHAR(250) NOT NULL,
     RichTextArea NVARCHAR(MAX) NULL,
-    ProductSubCategoryId INT NULL,
+    ProductCategoryId INT NOT NULL,
     CreationDate DATETIME NOT NULL DEFAULT (GETDATE()),
     LastUpdate DATETIME NULL DEFAULT (GETDATE()),
     CONSTRAINT PK_Products PRIMARY KEY CLUSTERED (Id ASC),
-    CONSTRAINT FK_Products_ProductSubCategories FOREIGN KEY (ProductSubCategoryId)
-        REFERENCES ProductSubCategories(Id)
-        ON DELETE CASCADE 
-        ON UPDATE CASCADE
+    CONSTRAINT FK_Products_ProductCategories FOREIGN KEY (ProductCategoryId) REFERENCES ProductCategories(Id)	ON DELETE CASCADE	ON UPDATE CASCADE
 );
